88038594622752f86cb96119f15f0cc1be713dc3
zsolt5553 <1034870@ucn.dk>
2015-11-05 11:29:56 +0100

    a

---
+++ PublicHospital/DataLayer/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class Person
+    {
+        private string firstName { get; set; }
+        private string lastName { get; set; }
+        private string city { get; set; }
+        private int zip { get; set; }
+        private string street { get; set; }
+        private int streetNr { get; set; }
+        private string phoneNr { get; set; }
+
+        public Person (string firstName, string lastName, string city, int zip, string street,
+            int streetNr, string phoneNr)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.city = city;
+            this.zip = zip;
+            this.street = street;
+            this.streetNr = streetNr;
+            this.phoneNr = phoneNr;
+        }
+    }
+}
