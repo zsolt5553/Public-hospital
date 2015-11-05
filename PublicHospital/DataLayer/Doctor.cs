8d5169d2b8342ef67a9e00f1106b10ce8b00190c
zsolt5553 <1034870@ucn.dk>
2015-11-05 11:18:01 +0100

    as

---
+++ PublicHospital/DataLayer/Doctor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    class Doctor : Person
+    {
+        private string specialty { get; set; }
+        private string description { get; set; }
+
+        public Doctor (string firstName, string lastName, string city, int zip, string street,
+            int streetNr, string phoneNr, string specialty, string description) :
+                base(firstName, lastName, city, zip, street, streetNr, phoneNr)
+        {
+            this.specialty = specialty;
+            this.description = description;
+        }
+    }
+}
