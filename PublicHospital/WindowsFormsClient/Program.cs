4bc03b921553f4db5d6ecac09fbc27c6c240e9f9
zsolt5553 <1034870@ucn.dk>
2015-11-19 14:24:31 +0100

    a

--- PublicHospital/WindowsFormsClient/Program.cs
+++ PublicHospital/WindowsFormsClient/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsClient
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new NewAdmin());
 
         }
     }
