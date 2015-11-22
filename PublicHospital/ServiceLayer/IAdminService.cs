5ccb02bb55183ed5826cc2aeec7a127bb052d417
adampap111 <lebron11@freemail.hu>
2015-11-22 19:52:11 +0100

    aaa

---
+++ PublicHospital/ServiceLayer/IAdminService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceLayer
+{
+    [ServiceContract]
+    public interface IAdminService
+    {
+        [OperationContract]
+        [FaultContract(typeof(AdminFault))]
+        Admin GetAdmin(int id);
+
+        [OperationContract]
+        [FaultContract(typeof(AdminFault))]
+        bool SaveAdmin(ref Admin admin,
+            ref string message);
+
+        [OperationContract]
+        [FaultContract(typeof(AdminFault))]
+        bool UpdateAdmin(ref Admin admin,
+            ref string message);
+    }
+
+    [DataContract]
+    public class Admin
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string firstName { get; set; }
+        [DataMember]
+        public string lastName { get; set; }
+        [DataMember]
+        public string city { get; set; }
+        [DataMember]
+        public int zip { get; set; }
+        [DataMember]
+        public string street { get; set; }
+        [DataMember]
+        public int streetNr { get; set; }
+        [DataMember]
+        public string phoneNr { get; set; }
+        [DataMember]
+        public string login { get; set; }
+        [DataMember]
+        public string pass { get; set; }
+    }
+    [DataContract]
+    public class AdminFault
+    {
+        public AdminFault(string msg)
+        {
+            FaultMessage = msg;
+        }
+
+        [DataMember]
+        public string FaultMessage;
+    }
+}
+
