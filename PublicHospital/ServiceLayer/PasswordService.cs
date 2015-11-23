c16fd7cc993057f03f2b139f94d185eb7959d3ac
adampap111 <lebron11@freemail.hu>
2015-11-23 09:20:14 +0100

    é

---
+++ PublicHospital/ServiceLayer/PasswordService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceLayer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PasswordService" in both code and config file together.
+    public class PasswordService : IPasswordService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
