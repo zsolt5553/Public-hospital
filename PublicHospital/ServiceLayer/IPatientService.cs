4d261c0eba1fe2cf483c5e8031b35f39e60d9184
zsolt5553 <1034870@ucn.dk>
2015-11-23 17:15:25 +0100

    a

---
+++ PublicHospital/ServiceLayer/IPatientService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceLayer
+{
+    [ServiceContract]
+    public interface IPatientService
+    {
+        [OperationContract]
+        [FaultContract(typeof(PatientFault))]
+        Patient GetPatient(int id);
+
+        [OperationContract]
+        [FaultContract(typeof(PatientFault))]
+        bool UpdatePatient(ref Patient Patient,
+            ref string message);
+    }
+
+    [DataContract]
+    public class Patient
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string firstName { get; set; }
+        [DataMember]
+        public string lastName { get; set; }
+        [DataMember]
+        public string city { get; set; }
+        [DataMember]
+        public int zip { get; set; }
+        [DataMember]
+        public string street { get; set; }
+        [DataMember]
+        public int streetNr { get; set; }
+        [DataMember]
+        public string phoneNr { get; set; }
+        [DataMember]
+        public DateTime dateOfBirth { get; set; }
+        [DataMember]
+        public string login { get; set; }
+        [DataMember]
+        public string pass { get; set; }
+    }
+    [DataContract]
+    public class PatientFault
+    {
+        public PatientFault(string msg)
+        {
+            FaultMessage = msg;
+        }
+
+        [DataMember]
+        public string FaultMessage;
+    }
+}
+
