6d0110922b4d8b4ce6fb0cd0f737f0e34ff8d3d1
adampap111 <lebron11@freemail.hu>
2015-11-29 11:11:29 +0100

    fefe

--- PublicHospital/WindowsFormsClient/DoctorMenu.Designer.cs
+++ PublicHospital/WindowsFormsClient/DoctorMenu.Designer.cs
@@ -33,52 +33,96 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(61, 44);
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Location = new System.Drawing.Point(243, 70);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(146, 43);
+            this.button1.Size = new System.Drawing.Size(183, 182);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Work calendar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(61, 93);
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(32, 70);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(146, 43);
+            this.button2.Size = new System.Drawing.Size(182, 182);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Patients list";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(61, 142);
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(32, 322);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(146, 43);
+            this.button3.Size = new System.Drawing.Size(182, 175);
             this.button3.TabIndex = 3;
-            this.button3.Text = "Patients history";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(61, 191);
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(243, 322);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(146, 43);
+            this.button4.Size = new System.Drawing.Size(183, 175);
             this.button4.TabIndex = 4;
-            this.button4.Text = "Logout";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel4.BackgroundImage")));
+            this.panel4.Location = new System.Drawing.Point(272, 284);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(119, 32);
+            this.panel4.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Location = new System.Drawing.Point(23, 284);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(191, 32);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.Location = new System.Drawing.Point(23, 32);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(191, 32);
+            this.panel2.TabIndex = 11;
+            // 
+            // panel3
+            // 
+            this.panel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel3.BackgroundImage")));
+            this.panel3.Location = new System.Drawing.Point(235, 32);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(191, 32);
+            this.panel3.TabIndex = 11;
             // 
             // DoctorMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(269, 265);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(201)))), ((int)(((byte)(227)))));
+            this.ClientSize = new System.Drawing.Size(464, 509);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel4);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -97,5 +141,9 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
     }
 }
