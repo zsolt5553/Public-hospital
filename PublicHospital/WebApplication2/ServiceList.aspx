37466100b082ce6a206e84b10d6bfeed1159bf51
Marek <lekon@o2.pl>
2015-12-05 10:01:57 +0100

    aaa

---
+++ PublicHospital/WebApplication2/ServiceList.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ServiceList.aspx.cs" Inherits="WebApplication2.SrviceList" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <h4 id="h4_1">Types of services</h4>
+        </div>
+        <div class="panel-body">
+            <div class="table-responsive">
+                <asp:table runat="server" ID="Tablee" CssClass="table table-striped"></asp:Table>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ PublicHospital/WebApplication2/ServiceList.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class SrviceList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int numrows = 3;
+            int numcells = 2;
+            for (int j = 0; j < numrows; j++)
+            {
+                TableRow r = new TableRow();
+                for (int i = 0; i < numcells; i++)
+                {
+                    TableCell c = new TableCell();
+                    c.Controls.Add(new LiteralControl("row "
+                        + j.ToString() + ", cell " + i.ToString()));
+                    r.Cells.Add(c);
+                }
+                Tablee.Rows.Add(r);
+            }
+        }
+    }
+}
